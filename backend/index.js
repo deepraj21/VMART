2208f3690f944893530666cb604ea8d7a25cb83c
Deepraj <deepraj21.bera@gmail.com>
2024-08-06 19:22:45 +0530

    Update backend API URLs to use the production server

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,3 @@
-// const express = require('express')
-// const app = express()
-// const port = 5000
-// const mongoDB = require("./db")
-// mongoDB();
-
-// app.use((req,res,next)=>{
-//     res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
-//     res.header(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept"
-//     );
-//     next();
-// })
-
-// app.get('/',(req,res) => {
-//     res.send('Hello World!')
-// })
-// app.use(express.json())
-// app.use('/api', require("./Routes/CreatUser"));
-// app.use('/api', require("./Routes/DisplayData"));
-// app.use('/api', require("./Routes/OrderData"));
-
-// app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}`)
-// })
-
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -35,7 +8,7 @@ mongoDB();
 // Define allowed origins
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://gofood-v1.netlify.app'
+    'https://vmart-v1.vercel.app/'
 ];
 
 // Use CORS middleware with dynamic origin
